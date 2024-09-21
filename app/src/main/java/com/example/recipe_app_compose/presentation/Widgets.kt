4fb04329255c1e0941870877f982a592a1a1ee4a
stefan bayne <stefanbayne@gmail.com>
2024-09-21 14:53:27 -0400

    bugfix: addressed corner radius issue.

--- app/src/main/java/com/example/recipe_app_compose/presentation/Widgets.kt
+++ app/src/main/java/com/example/recipe_app_compose/presentation/Widgets.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.MaterialTheme
@@ -99,7 +100,7 @@ fun MyBottomAppBar(
         color = containerColor,
         contentColor = contentColor,
         tonalElevation = tonalElevation,
-        //shape = ShapeKeyTokens.CornerNone,
+        shape = RoundedCornerShape(40.dp),
         modifier = modifier
     ) {
         Row(
