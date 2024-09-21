fdcb5c5f41bcd14112234f084a55ef5216a8dcca
stefan bayne <stefanbayne@gmail.com>
2024-09-21 10:32:11 -0400

    fix: change bottom sheet text to match design.

--- app/src/main/java/com/example/recipe_app_compose/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/recipe_app_compose/presentation/view/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,10 +27,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.recipe_app_compose.presentation.AlertDialogExample
 import com.example.recipe_app_compose.presentation.FullScreenDialog
 import com.example.recipe_app_compose.ui.theme.Recipe_App_ComposeTheme
+import androidx.compose.ui.unit.sp as sp
 
 class MainActivity : ComponentActivity() {
 
@@ -104,9 +110,14 @@ class MainActivity : ComponentActivity() {
                             Column {
                                 Text(
                                     "Seafood Sheet",
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 20.sp
+                                    ),
                                     textAlign = TextAlign.Center,
                                     modifier = Modifier.fillMaxWidth()
                                 )
+                                Spacer(modifier = Modifier.padding(top = 20.dp))
                                 SeafoodRecipeScreen(modifier = Modifier.fillMaxSize())
                             }
                         }
