2185e114fb746347243cf781476acefa00f83d4d
stefan bayne <stefanbayne@gmail.com>
2024-09-27 12:20:22 -0400

    fix: bugfixing

--- app/src/main/java/com/example/recipe_app_compose/features/categories/data/local/db/RandomMealDatabase.kt
+++ app/src/main/java/com/example/recipe_app_compose/features/categories/data/local/db/RandomMealDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.recipe_app_compose.features.categories.domain.model.randommeal.RandomMeal
 
-@Database(entities = [RandomMeal::class], version = 2, exportSchema = false)
+@Database(entities = [RandomMeal::class], version = 4, exportSchema = false)
 abstract class RandomMealDatabase : RoomDatabase() {
     abstract fun randomMealDao(): RandomMealDAO
 }
