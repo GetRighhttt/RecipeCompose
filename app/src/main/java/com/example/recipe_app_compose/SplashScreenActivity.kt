94ea38e998a25624518ca89404f38546d191949b
stefan bayne <stefanbayne@gmail.com>
2024-10-01 10:22:10 -0400

    feat: adding animation

--- app/src/main/java/com/example/recipe_app_compose/SplashScreenActivity.kt
+++ app/src/main/java/com/example/recipe_app_compose/SplashScreenActivity.kt
@@ -6,16 +6,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import kotlinx.coroutines.delay
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : ComponentActivity() {
@@ -31,9 +34,13 @@ class SplashScreenActivity : ComponentActivity() {
 @Composable
 fun SplashScreen() {
     val context = LocalContext.current
+    val alpha = remember {
+        Animatable(0F)
+    }
+
     // Coroutine Launcher that initiates when composables are composed
     LaunchedEffect(key1 = true, block = {
-        delay(1000)
+        alpha.animateTo(1F, animationSpec = tween(1500))
         context.startActivity(Intent(context, LoginActivity::class.java))
     })
     Box(
@@ -41,6 +48,7 @@ fun SplashScreen() {
         contentAlignment = Alignment.Center
     ) {
         Image(
+            modifier = Modifier.alpha(alpha = alpha.value),
             painter = painterResource(R.drawable.dining_two),
             contentDescription = null)
     }
