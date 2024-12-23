2d36a7869352bc3014b055c8ef848917edd35e32
stefan bayne <stefanbayne@gmail.com>
2024-12-22 19:50:41 -0500

    fix: updating target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.recipe_app_compose"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
