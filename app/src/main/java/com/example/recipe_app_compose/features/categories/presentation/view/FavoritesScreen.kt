a2ce4f4c40fa6084ba22d3d3e939fc6b8abeaaba
stefan bayne <stefanbayne@gmail.com>
2024-12-23 19:56:11 -0500

    feat: optimize imports

--- app/src/main/java/com/example/recipe_app_compose/features/categories/presentation/view/FavoritesScreen.kt
+++ app/src/main/java/com/example/recipe_app_compose/features/categories/presentation/view/FavoritesScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -38,7 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
