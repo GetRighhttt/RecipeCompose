7979490332df274103981dcecbdd3cffb08bead0
stefan bayne <stefanbayne@gmail.com>
2024-12-30 07:46:49 -0500

    feat: securing api keys and base url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     // Existing plugins
     alias(libs.plugins.compose.compiler)
@@ -23,8 +25,17 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField("String", "YELP_API_KEY",properties.getProperty("YELP_API_KEY"))
+        buildConfigField("String","YELP_BASE_URL",properties.getProperty("YELP_BASE_URL"))
+        buildConfigField("String","BASE_URL",properties.getProperty("BASE_URL"))
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
--- app/src/main/java/com/example/recipe_app_compose/core/util/Constants.kt
+++ app/src/main/java/com/example/recipe_app_compose/core/util/Constants.kt
@@ -1,13 +1,14 @@
 package com.example.recipe_app_compose.core.util
 
+import com.example.recipe_app_compose.BuildConfig
+
 object Constants {
-    const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+    const val BASE_URL = BuildConfig.BASE_URL
     const val CATEGORY_ENDPOINT = "categories.php"
     const val CATEGORY_MEAL_ENDPOINT = "filter.php"
     const val RANDOM_MEAL_ENDPOINT = "random.php"
     const val INGREDIENT_ENDPOINT = "search.php"
-    const val YELP_API_KEY =
-        "vbM-Gqh-Gn7sF6frR9SEWe9O7gNnqB1gZadN8qLfiyPhDITiF5fySBMdZ9c7qaowT33t8WB05lziTfFmG3mRsx2iPEHoBMiws562ybGBrLfcJ3t4x9d_k-PhuhTzZnYx"
-    const val YELP_BASE_URL = "https://api.yelp.com/v3/"
+    const val YELP_API_KEY = BuildConfig.YELP_API_KEY
+    const val YELP_BASE_URL = BuildConfig.YELP_BASE_URL
     const val YELP_SEARCH_QUERY = "Steak in Wesley Chapel"
 }
