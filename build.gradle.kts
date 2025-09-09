7a09e6a55e91a9ecea6dd6961e68498e30610726
Stefan Bayne <105057858+GetRighhttt@users.noreply.github.com>
2025-09-09 04:14:55 -0500

    Upgrading dependencies and plugins (#109)
    Co-authored-by: Stefan Bayne <stefanbayne@Stefans-MacBook-Air.attlocal.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("androidx.room") version "2.6.1" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    id("androidx.room") version "2.7.2" apply false
+    id("com.google.devtools.ksp") version "2.2.20-RC2-2.0.2" apply false
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
     // Add the dependency for the Performance Monitoring Gradle plugin
-    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+    id("com.google.firebase.firebase-perf") version "2.0.1" apply false
 }
